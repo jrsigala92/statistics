afc8f780a753d46afff437128538a082c9274bab
JR <jsigala92@outlook.com>
2020-02-26 21:53:24 -0700

    adding services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HttpClientModule} from '@angular/common/http';
+import { GroupComponent } from './components/group/group.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GroupComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
